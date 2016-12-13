9bb17a8e0d3690bcc17e24249ae395293b93482c
dpysmennyi <dpysmennyi@corevalue.net>
2016-12-13 19:40:16 +0200

    Update data

--- routes/topic.js
+++ routes/topic.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var Topic = require('./../model/topicModel');
 var router = express.Router();
+var fs = require('fs');
 
-var topics = [
-    new Topic('test', 'me', 'this is text', new Date())
-];
+var topics = initTopics();
 
 router
     .get('/', function (req, res, next) {
@@ -31,7 +30,11 @@ router
         res.sendStatus(201);
     })
     .put('/:name/', function (req, res, next) {
-        //TODO
+        var body = req.body;
+        topics = topics.filter(function (el) {
+            return el.name !== req.params.name
+        });
+        topics.push(new Topic(body.name, body.author, body.text, body.date));
     })
     .delete('/:name', function (req, res, next) {
         var name = req.params.name;
@@ -41,5 +44,14 @@ router
         });
         res.send(200);
     });
+
+function initTopics() {
+    var topics = [];
+    var topicsData = JSON.parse(fs.readFileSync('./data/topics.json'));
+    topicsData.topics.forEach(function (element) {
+        topics.push(new Topic(element.name, element.author, element.text, element.data));
+    });
+    return topics;
+}
 
 module.exports = router;
